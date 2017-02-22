d50e5c9b6c1d70e234a30ac997fd0c6e0121a08d
joe <sheninter@gmail.com>
2017-02-22 16:00:15 +0800

    Initial Commit

---
+++ ChinsesNumberalsFormat/ChinsesNumberalsFormat.h
@@ -0,0 +1,19 @@
+//
+//  ChinsesNumberalsFormat.h
+//  ChinsesNumberalsFormat
+//
+//  Created by joe on 2/22/17.
+//  Copyright © 2017 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChinsesNumberalsFormat.
+FOUNDATION_EXPORT double ChinsesNumberalsFormatVersionNumber;
+
+//! Project version string for ChinsesNumberalsFormat.
+FOUNDATION_EXPORT const unsigned char ChinsesNumberalsFormatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChinsesNumberalsFormat/PublicHeader.h>
+
+
